b20e9cdd417451b27fc74e3a70f61d6b2ad42f9e
EvgenyPerevera <149911979+EvgenyPerevera@users.noreply.github.com>
2025-05-22 13:25:50 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,19 @@
+-- считает общее количество покупателей из таблицы customers и присваивает колонке псевдоним customers_count
 SELECT
-    COUNT(*) AS customers_count  -- считает общее количество покупателей из таблицы customers и присваивает колонке псевдоним customers_count
+    COUNT(*) AS customers_count  
 FROM customers;
+
+
+--
+SELECT
+    CONCAT(e.first_name, ' ', e.last_name) AS seller,
+    COUNT(s.sales_id) AS operations,
+    SUM(p.price * s.quantity) AS income
+From sales s
+JOIN
+    employees e ON s.sales_person_id = e.employee_id
+JOIN
+    products p ON s.product_id = p.product_id
+GROUP by 1
+ORDER by 3 DESC
+LIMIT 10;

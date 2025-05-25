dbad919a17e995c11ddcc5da516d82b9be1cd19a
EvgenyPerevera <149911979+EvgenyPerevera@users.noreply.github.com>
2025-05-25 11:05:35 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -53,3 +53,19 @@ ORDER BY
         ELSE EXTRACT(DOW FROM s.sale_date)            
    END, 
     1;
+
+
+
+
+
+--
+SELECT
+  CASE
+    WHEN age BETWEEN 16 AND 25 THEN '16-25'
+    WHEN age BETWEEN 26 AND 40 THEN '26-40'
+    ELSE '40+'
+  END AS age_category,
+  COUNT(DISTINCT customer_id) AS age_count
+FROM customers
+GROUP BY 1
+ORDER by 1;

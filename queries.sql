dacbb67e501203de1e25cc5a33cbc225901b4e89
EvgenyPerevera <149911979+EvgenyPerevera@users.noreply.github.com>
2025-06-03 15:29:01 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,16 +1,16 @@
 -- Общее количество покупателей
 SELECT
     COUNT(*) AS customers_count
-FROM customers;
+FROM public.customers;
 
 -- Топ-10 продавцов по выручке
 SELECT
     CONCAT(e.first_name, ' ', e.last_name) AS seller,
     COUNT(s.sales_id) AS operations,
     FLOOR(SUM(p.price * s.quantity)) AS income
-FROM sales AS s
-JOIN employees AS e ON s.sales_person_id = e.employee_id
-JOIN products AS p ON s.product_id = p.product_id
+FROM public.sales AS s
+JOIN public.employees AS e ON s.sales_person_id = e.employee_id
+JOIN public.products AS p ON s.product_id = p.product_id
 GROUP BY seller
 ORDER BY income DESC
 LIMIT 10;
@@ -19,16 +19,17 @@ LIMIT 10;
 SELECT
     CONCAT(e.first_name, ' ', e.last_name) AS seller,
     ROUND(AVG(p.price * s.quantity)) AS average_income
-FROM sales AS s
-JOIN employees AS e ON s.sales_person_id = e.employee_id
-JOIN products AS p ON s.product_id = p.product_id
+FROM public.sales AS s
+JOIN public.employees AS e ON s.sales_person_id = e.employee_id
+JOIN public.products AS p ON s.product_id = p.product_id
 GROUP BY seller
-HAVING AVG(p.price * s.quantity) < (
-    SELECT
-        AVG(p2.price * s2.quantity)
-    FROM sales AS s2
-    JOIN products AS p2 ON s2.product_id = p2.product_id
-)
+HAVING
+    AVG(p.price * s.quantity) < (
+        SELECT
+            AVG(p2.price * s2.quantity)
+        FROM public.sales AS s2
+        JOIN public.products AS p2 ON s2.product_id = p2.product_id
+    )
 ORDER BY average_income;
 
 -- Выручка продавцов по дням недели
@@ -36,10 +37,13 @@ SELECT
     CONCAT(e.first_name, ' ', e.last_name) AS seller,
     TO_CHAR(s.sale_date, 'Day') AS day_of_week,
     FLOOR(SUM(p.price * s.quantity)) AS income
-FROM sales AS s
-JOIN employees AS e ON s.sales_person_id = e.employee_id
-JOIN products AS p ON s.product_id = p.product_id
-GROUP BY seller, day_of_week, EXTRACT(DOW FROM s.sale_date)
+FROM public.sales AS s
+JOIN public.employees AS e ON s.sales_person_id = e.employee_id
+JOIN public.products AS p ON s.product_id = p.product_id
+GROUP BY
+    seller,
+    day_of_week,
+    EXTRACT(DOW FROM s.sale_date)
 ORDER BY
     CASE
         WHEN EXTRACT(DOW FROM s.sale_date) = 0 THEN 7
@@ -55,7 +59,7 @@ SELECT
         ELSE '40+'
     END AS age_category,
     COUNT(DISTINCT c.customer_id) AS age_count
-FROM customers AS c
+FROM public.customers AS c
 GROUP BY age_category
 ORDER BY age_category;
 
@@ -64,8 +68,8 @@ SELECT
     TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month,
     COUNT(DISTINCT s.customer_id) AS total_customers,
     FLOOR(SUM(s.quantity * p.price)) AS income
-FROM sales AS s
-JOIN products AS p ON s.product_id = p.product_id
+FROM public.sales AS s
+JOIN public.products AS p ON s.product_id = p.product_id
 GROUP BY selling_month
 ORDER BY selling_month;
 
@@ -74,9 +78,9 @@ SELECT DISTINCT ON (c.customer_id)
     c.first_name || ' ' || c.last_name AS customer,
     s.sale_date,
     e.first_name || ' ' || e.last_name AS seller
-FROM sales AS s
-JOIN customers AS c ON s.customer_id = c.customer_id
-JOIN products AS p ON s.product_id = p.product_id
-JOIN employees AS e ON s.sales_person_id = e.employee_id
+FROM public.sales AS s
+JOIN public.customers AS c ON s.customer_id = c.customer_id
+JOIN public.products AS p ON s.product_id = p.product_id
+JOIN public.employees AS e ON s.sales_person_id = e.employee_id
 WHERE p.price = 0
 ORDER BY c.customer_id, s.sale_date;
